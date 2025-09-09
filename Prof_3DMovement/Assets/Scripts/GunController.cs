d8b568ac6fa2436b5e94e78d2fcee61104947282
Sebastien Siva <ssiva@ggc.edu>
2025-09-09 18:14:55 -0400

    Lecture 9_9_25

---
+++ Prof_3DMovement/Assets/Scripts/GunController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject g = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            g.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        }
+    }
+}
---
+++ Prof_3DMovement/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = transform.forward * v + transform.right * h;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
